e8cab76bd30917d43a21623bc729b90ed7c44351
randName <kwokshungit@gmail.com>
2013-09-07 18:50:12 +0800

    added accelerometer readout and signed type handling

--- compass.py
+++ compass.py
@@ -1,4 +1,5 @@
 import smbus
+import ctypes
 
 class Compass:
 
@@ -10,21 +11,29 @@ class Compass:
 
 	def bearing(self):
 		if self.onebyte:
-			return self.bus.read_byte_data(self.address, 1)
+			return self.bus.read_byte_data(self.address,1)
 		else:
 			
-			b1 = self.bus.read_byte_data(self.address, 2)
-			b2 = self.bus.read_byte_data(self.address, 3)
+			b1 = self.bus.read_byte_data(self.address,2)
+			b2 = self.bus.read_byte_data(self.address,3)
 			return ( b1 << 8 ) + b2
 
 	def zero(self):
 		self.error = self.maxval - self.bearing(self.onebyte)
 
 	def pitch(self):
-		return self.bus.read_byte_data(self.address, 4)
+		return ctypes.c_byte(self.bus.read_byte_data(self.address,4)).value
 
 	def roll(self):
-		return self.bus.read_byte_data(self.address, 5)
+		return ctypes.c_byte(self.bus.read_byte_data(self.address,5)).value
+
+	def accelerometer(self):
+		vals = []
+		for x in range(3): 
+			b1 = self.bus.read_byte_data(self.address,x*2+16)
+			b2 = self.bus.read_byte_data(self.address,x*2+17)
+			vals.append( ctypes.c_int16((b1<<8)+b2).value )
+		return vals
 
 	def read(self):
 		return self.bearing() + self.error
